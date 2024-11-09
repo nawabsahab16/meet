5520f2b0146d77d4c9d552d8b2619c14e7be26ba
nawabsahab16 <Nawab162004@gmail.com>
2024-11-09 11:40:50 +0530

    feat: configure NextAuth with GitHub and Google providers, user database handling, and custom callbacks

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,22 +1,71 @@
-import NextAuth from "next-auth";
+import dbConnect from "@/lib/dbConnect";
+import User from "@/models/User";
+import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google";
 
-
 export const authOptions = {
-    providers: [
-        GithubProvider({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET,
-        }),
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        })
-    ],
  
-    
+    providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
 
+    GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET
+      })
+  ],
+
+  callbacks:{
+    async jwt({token,user,account}){
+
+        if(user){
+            token.id = user.id;
+        }
+        if(account){
+            token.accessToken= account.access_token;
+        }
+
+        return token;
+    },
+
+    async session({ session, token }) {
+         session.user.id= token.id;
+        return session
+      },
+
+      async signIn({user,profile}) {
+
+        await dbConnect();
+
+        let dbUser = await User.findOne({email: user.email})
+
+        if(!dbUser){
+
+            dbUser = await User.create({
+                name:profile.name,
+                email:profile.email,
+                profilePicture:profile.picture,
+                isVerified:profile.email_verified ? true :false
+            })
+        }
+
+         user.id = dbUser._id.toString();
+         return true;
+      }
+
+  },
+  
+  session:{
+    strategy: 'jwt',
+    maxAge : 90 * 24 * 60 * 60
+  },
+  pages:{
+    signIn: '/user-auth',
+  }
 }
 
-export default NextAuth(authOptions)
+const handle = NextAuth(authOptions)
+export {handle as POST , handle as GET};
